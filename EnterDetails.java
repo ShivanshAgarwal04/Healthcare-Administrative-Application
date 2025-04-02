044ccda7a78da5fc325cff5b99d0c5f42c205654
Meldon <my269@kent.ac.uk>
2025-04-02 17:39:46 +0100

    completed view prescriptions and view details

--- EnterDetails.java
+++ EnterDetails.java
@@ -4,34 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 
-// Database Connection
-class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Younm070.306";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-    public static boolean confirmBookingID(Integer bookingNo){
-        boolean isValid = false;
 
-        try (Connection conn = getConnection();
-             PreparedStatement statement = conn.prepareStatement("SELECT bookingNo FROM Bookings WHERE bookingNo = ?")) {
-            statement.setInt(1, bookingNo);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    isValid = (bookingNo.equals(resultSet.getInt("bookingNo")));
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return isValid;
-    }
-}
 
-// Main window
 public class EnterDetails extends JFrame {
     public EnterDetails() {
         setTitle("Enter visit details & prescriptions");
