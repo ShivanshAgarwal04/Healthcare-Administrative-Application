ceea94a69d1514609e2829ac322e4602aa2d6f69
Meldon <my269@kent.ac.uk>
2025-04-02 17:58:15 +0100

    DB functions - We need to merge DB files at some point

---
+++ db.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+// Database Connection
+class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    public static boolean confirmBookingID(Integer bookingNo){
+        boolean isValid = false;
+
+        try (Connection conn = getConnection();
+             PreparedStatement statement = conn.prepareStatement("SELECT bookingNo FROM Bookings WHERE bookingNo = ?")) {
+            statement.setInt(1, bookingNo);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    isValid = (bookingNo.equals(resultSet.getInt("bookingNo")));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isValid;
+    }
+}
